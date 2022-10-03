ed54db4be3a69a13aed0f58f873f44c4a1ccfd4d
known-as-bmf <bmf.geekinside@gmail.com>
2022-10-03 18:08:58 +0200

    chore: work on homepage

--- themes/digibloom/tailwind.config.js
+++ themes/digibloom/tailwind.config.js
@@ -1,14 +1,47 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+const customColors = {
+  digibleu: {
+    light: '#EDFFFE',
+    DEFAULT: '#D0F9F6',
+    normal: '#D0F9F6',
+    dark: '#8BBFBC',
+  },
+  digijaune: {
+    DEFAULT: '#FFF5B6',
+    normal: '#FFF5B6',
+    dark: '#E0C72F',
+  },
+};
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../../content/**/*.md", "../../layouts/**/*.html", "content/**/*.md", "layouts/**/*.html"],
+  content: [
+    '../../content/**/*.md',
+    '../../content/**/*.html',
+    '../../layouts/**/*.html',
+    'content/**/*.md',
+    'layouts/**/*.html',
+  ],
   theme: {
     extend: {
+      colors: customColors,
       fontFamily: {
-        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            p: {
+              textAlign: 'justify',
+            },
+            figcaption: {
+              textAlign: 'center',
+            },
+          },
+        },
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require('@tailwindcss/typography')],
 };
