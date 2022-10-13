a44f35c924e2a8bb75b3b735356e31ddc02bdc92
known-as-bmf <bmf.geekinside@gmail.com>
2022-10-13 21:27:23 +0200

    chore: wip

--- themes/digibloom/tailwind.config.js
+++ themes/digibloom/tailwind.config.js
@@ -36,6 +36,9 @@ module.exports = {
             p: {
               textAlign: 'justify',
             },
+            li: {
+              textAlign: 'justify',
+            },
             figcaption: {
               textAlign: 'center',
             },
