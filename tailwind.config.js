a6dbe87b047c4b18fe894d37ebd97f7155df08b2
Julien Avert <bmf.geekinside@gmail.com>
2022-11-05 15:53:48 +0100

    chore: wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,11 +22,11 @@ const customColors = {
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '../../content/**/*.md',
-    '../../content/**/*.html',
-    '../../layouts/**/*.html',
     'content/**/*.md',
+    'content/**/*.html',
     'layouts/**/*.html',
+    'themes/digibloom/content/**/*.md',
+    'themes/digibloom/layouts/**/*.html',
   ],
   theme: {
     extend: {
